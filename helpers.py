7b56062bb257f129080e9e400ac6db866a042c20
Mariusz Matyja <newmatyjamariusz@gmail.com>
2024-11-14 22:39:05 +0100

    figure generator supports rand background color

--- helpers.py
+++ helpers.py
@@ -4,6 +4,35 @@ import cv2
 
 import numpy as np
 
+def calc_rgb_diff(c1,c2):
+    # calc distance between towo rgb colors:
+    c1 = [int(x) for x in c1]
+    return np.sqrt((c1[0]-c2[0])**2 + (c1[1]-c2[1])**2 + (c1[2]-c2[2])**2)
+
+
+def get_rand_rgb(min,max):
+    return (np.random.randint(min, max, dtype=np.uint8), 
+            np.random.randint(min, max, dtype=np.uint8),  
+            np.random.randint(min, max, dtype=np.uint8)
+            )
+
+def get_distinct_t_bg_colors():
+    # get distinct colors for text and background
+    t_color = get_rand_rgb(0, 150)
+    bg_color = get_rand_rgb(150, 255)
+    while calc_rgb_diff(t_color, bg_color) < 150:
+        bg_color = get_rand_rgb(150, 255)
+    return t_color, bg_color
+
+def mk_bg_img(img_size, bg_color):
+    # Create a black image
+    img = np.zeros((img_size, img_size, 3), np.uint8)
+    img += bg_color
+    return img
+
+
+
+
 def scatter_point(img, x, y, size):
     y_low_lim = max(0, y-size)
     y_high_lim = min(img.shape[0], y+size)
@@ -16,60 +45,67 @@ def scatter_point(img, x, y, size):
 def get_files_in_dir(path):
     return [f for f in listdir(path) if isfile(join(path, f))]
 
-def get_rand_rgb(min,max):
-    return (np.random.randint(min, max), np.random.randint(min, max), np.random.randint(min, max))
 
-def draw_random_circle(img_size, radius_range, thickness_range):    # Create a black image
-    img = np.zeros((img_size, img_size, 3), np.uint8)
+def draw_random_circle(img_size, radius_range, thickness_range):    
+    t_color, bg_color = get_distinct_t_bg_colors()
+    t_color = [int(x) for x in t_color]
+    # Create a black image
+    img = mk_bg_img(img_size, bg_color)
     # Draw a circle
-    center = (np.random.randint(0, img_size), np.random.randint(0, img_size))
+    center = (np.random.randint(img_size//8, img_size//1.3), np.random.randint(img_size//8, img_size//1.3))
     radius = np.random.randint(*radius_range)
-    # random color
-    color = get_rand_rgb(15, 250)
     thickness = np.random.randint(*thickness_range)
-    img = cv2.circle(img, center, radius, color, thickness)
+    img = cv2.circle(img, center, radius,
+                      color = t_color,
+                      thickness= thickness)
     return img
 
 def draw_random_rectangle(img_size, thickness_range):
+    t_color, bg_color = get_distinct_t_bg_colors()
+    t_color = [int(x) for x in t_color]
     # Create a black image
-    img = np.zeros((img_size, img_size, 3), np.uint8)
+    img = mk_bg_img(img_size, bg_color)
     # Draw a rectangle
-    pt1 = (np.random.randint(0, img_size), np.random.randint(0, img_size))
-    pt2 = (np.random.randint(0, img_size), np.random.randint(0, img_size))
-    color = get_rand_rgb(15, 250)
+    pt1 = (np.random.randint(img_size//10, img_size//2), np.random.randint(img_size//10, img_size//2))
+    pt2 = (np.random.randint(img_size//2.5, img_size//1.2), np.random.randint(img_size//2.5, img_size//1.2))
     thickness = np.random.randint(*thickness_range)
-    img = cv2.rectangle(img, pt1, pt2, color, thickness)
+    img = cv2.rectangle(img, pt1, pt2, t_color, thickness)
     return img
 
 def draw_random_elipse(img_size, thickness_range):
+    t_color, bg_color = get_distinct_t_bg_colors()
+    t_color = [int(x) for x in t_color]
     # Create a black image
-    img = np.zeros((img_size, img_size, 3), np.uint8)
+    img = mk_bg_img(img_size, bg_color)
     # Draw a rectangle
-    pt1 = (np.random.randint(img_size//10, img_size//1.2), np.random.randint(img_size//10, img_size//1.2))
+    pt1 = (np.random.randint(img_size//8, img_size//1.2), np.random.randint(img_size//8, img_size//1.2))
     pt2 = (np.random.randint(img_size//8,img_size//2), np.random.randint(img_size//8,img_size//2))
 
     rand_angle = np.random.randint(0, 360)
-    color = get_rand_rgb(15, 250)
+
     thickness = np.random.randint(*thickness_range)
-    img = cv2.ellipse(img, pt1, pt2, rand_angle, 0, 360, color, thickness)
+    img = cv2.ellipse(img, pt1, pt2, rand_angle, 0, 360, t_color, thickness)
     return img
 
 def draw_random_line(img_size, thickness_range):
+    t_color, bg_color = get_distinct_t_bg_colors()
+    t_color = [int(x) for x in t_color]
     # Create a black image
-    img = np.zeros((img_size, img_size, 3), np.uint8)
+    img = mk_bg_img(img_size, bg_color)
     # Draw a rectangle
-    pt1 = (np.random.randint(0, img_size), np.random.randint(0, img_size))
-    pt2 = (np.random.randint(0, img_size), np.random.randint(0, img_size))
-    color = get_rand_rgb(15, 250)
+    pt1 = (np.random.randint(img_size//10, img_size//2), np.random.randint(img_size//10, img_size//2))
+    pt2 = (np.random.randint(img_size//2.5, img_size//1.2), np.random.randint(img_size//2.5, img_size//1.2))
+
     thickness = np.random.randint(*thickness_range)
-    img = cv2.line(img, pt1, pt2, color, thickness)
+    img = cv2.line(img, pt1, pt2, t_color, thickness)
     return img
 
 def draw_random_triangle(img_size, thickness_range):
+    t_color, bg_color = get_distinct_t_bg_colors()
+    t_color = [int(x) for x in t_color]
     # Create a black image
-    img = np.zeros((img_size, img_size, 3), np.uint8)
+    img = mk_bg_img(img_size, bg_color)
     # Draw a rectangle
-    #50,70
     x1,y1 = np.random.randint(2, img_size), np.random.randint(2, img_size)
 
     x2_lim = min(x1+1, img_size)    
@@ -88,9 +124,9 @@ def draw_random_triangle(img_size, thickness_range):
         return draw_random_triangle(img_size, thickness_range)
     #print(area)
 
-    color = get_rand_rgb(15, 250)
+
     thickness = np.random.randint(*thickness_range)
-    img = cv2.polylines(img, [pts], isClosed=True, color=color, thickness=thickness)
+    img = cv2.polylines(img, [pts], isClosed=True, color=t_color, thickness=thickness)
     return img
 
 
@@ -109,23 +145,23 @@ def draw_random_figure(img_size, thickness_range):
         return fig,draw_random_triangle(img_size, thickness_range)
 
 
-import matplotlib.pyplot as plt
-# circle_img = draw_random_circle(100, (5, 30), (1, 10))
-# plt.imshow(circle_img)
-# plt.show()
+# # import matplotlib.pyplot as plt
+# # circle_img = draw_random_circle(100, (5, 30), (1, 10))
+# # plt.imshow(circle_img)
+# # plt.show()
 
-# rectangle_img = draw_random_rectangle(100, (1, 5))
-# plt.imshow(rectangle_img)
-# plt.show()
+# # rectangle_img = draw_random_rectangle(100, (1, 5))
+# # plt.imshow(rectangle_img)
+# # plt.show()
 
-# elipse_img = draw_random_elipse(100, (1, 5))
-# plt.imshow(elipse_img)
-# plt.show()
+# # elipse_img = draw_random_elipse(100, (1, 5))
+# # plt.imshow(elipse_img)
+# # plt.show()
 
-# line_img = draw_random_line(224, (1, 10))
-# plt.imshow(line_img)
-# plt.show()
+# # line_img = draw_random_line(224, (1, 10))
+# # plt.imshow(line_img)
+# # plt.show()
 
-# polygon_img = draw_random_triangle(224, (1, 8))
-# plt.imshow(polygon_img)
-# plt.show()
+# # polygon_img = draw_random_triangle(224, (1, 8))
+# # plt.imshow(polygon_img)
+# # plt.show()
